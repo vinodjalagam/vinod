818d3ccd28ce7d72e5943f31865aa546f107688c
vinodjalagam <63002962+vinodjalagam@users.noreply.github.com>
2024-02-28 22:23:08 +0530

    Update hello.sh
    I changed some line

--- hello.sh
+++ hello.sh
@@ -2,3 +2,10 @@
 echo "hello and good morning"
 echo "hello bro"
 echo "welcome to the project team"
+#!/bin/bash
+echo "hello and good morning"
+echo "hello bro"
+echo "welcome to the project team"#!/bin/bash
+echo "hello and good morning"
+echo "hello bro"
+echo "welcome to the project team"
